31e3a0b57e908938fc1f2e98fd646d5cbe0ded5b
R W <rrwen.dev@gmail.com>
2022-09-11 22:10:36 -0400

    Note for dstruct of params

--- src/data.py
+++ src/data.py
@@ -58,9 +58,11 @@ def gen_grid(bounds, cells=None, cell_width=None, cell_height=None):
     return out
 
 def df2params(df, constraints=None):
+    # TODO
     # x1 = df.query('col == "value"').col
     # idx = x1.index.tolist()
     # df.loc[idx, 'col'] = ['val1', 'val2']
+    # out = dict(col1 = {start: 0, end: 5, values=[1,2,3,4,5], constraints={}}, col2 = {})
     pass
 
 def params2df():
